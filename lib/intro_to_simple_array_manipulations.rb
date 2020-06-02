a2a4d19213446bd352ca37417cbc74cbde3599e2
Anh Vuong <vuonga1103@yahoo.com>
2020-06-02 16:02:39 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr, elem)
+  arr.insert(4, elem)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+
+def using_delete(arr, str)
+  arr.delete(str)
+end
+
+def using_delete_at(arr, int)
+  arr.delete_at(int)
+end
